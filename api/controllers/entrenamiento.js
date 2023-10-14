097ee484ba1328fe5aec830868893740b719f6a7
pabloangusto <pabloangusto@gmail.com>
2023-10-14 17:41:58 +0200

    ns

--- api/controllers/entrenamiento.js
+++ api/controllers/entrenamiento.js
@@ -1,5 +1,6 @@
 
 const Training = require("../models/trainingSchema")
+const Exercise = require("../models/exerciseSchema")
 
 
 const prueba = (req, res) => {
@@ -9,12 +10,50 @@ const prueba = (req, res) => {
     })
 }
 
-const add = (req, res) => {
+const add = async(req, res) => {
     
+    let sets = req.body.sets
+    
+    if(sets.length == 0 ){
+        return res.status(500).send({
+            status:"error",
+            message: "No se puede guardar una rutina con 0 ejercicios"
+        })
+    }
+    let exercisesIds = sets.map(set => set.exercise)
+    let exercisesFound = await Exercise.find({_id: {$in: exercisesIds}})
+    
+    if(exercisesFound.length != sets.length){
+        return res.status(500).send({
+            status:"error",
+            message: "Error en al menos un ejercicio"
+        })
+    }
+
+    let newTraining = new Training({name: req.body.name, user: req.user.id, sets: req.body.sets})
+
+    let trainingSaved = await newTraining.save()
+    if(!trainingSaved){
+        return res.status(500).send({
+            status:"error",
+            message: "No se ha podido guardar el entrenamiento"
+        })
+    }
+    return res.status(200).send({
+        status:"success",
+        message: "Se ha guardado el entrenamiento correctamente",
+        trainingSaved
+    })
+
+
+
+    
+
 }
 
 
 
 module.exports = {
-    prueba
+    prueba,
+    add
 }
