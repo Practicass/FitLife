841e38b8c4d7ed26ef66c97d2f03ad2ba6975b22
pabloangusto <pabloangusto@gmail.com>
2023-10-15 16:10:22 +0200

    fotos perfil

--- api/controllers/usuario.js
+++ api/controllers/usuario.js
@@ -1,7 +1,11 @@
 
 const User = require("../models/usersSchema")
+const Friend = require("../models/friendSchema")
+const Training = require("../models/trainingSchema")
 const bcrypt = require("bcrypt")
 const jwt = require("../services/jwt")
+const fs = require("fs");
+const path = require("path");
 
 const friendService = require("../services/friendService")
 
@@ -180,24 +184,183 @@ const list = (req, res) => {
     
   }
 
-//funcion que devuelve la imágen de un usuario
-const avatar = ( req, res) => {
-    
-}
 
-//funcion que elimina un usuario
-const deleteUser = (req, res) => {
-    
-}
 
 // funcion que modifica un usuario
-const modUser = ( req, res) => {
-    
+const update = (req, res) => {
+    // Recoger info del usuario a actualizar
+    let userIdentity = req.user;
+    let userToUpdate = req.body;
+
+    // Eliminar campos sobrantes
+    delete userToUpdate.iat;
+    delete userToUpdate.exp;
+    delete userToUpdate.role;
+    delete userToUpdate.image;
+
+    // Comprobar si el usuario ya existe
+    User.find({
+        $or: [
+            { email: userToUpdate.email.toLowerCase() },
+            { nick: userToUpdate.nick.toLowerCase() }
+        ]
+    }).exec(async (error, users) => {
+
+        if (error) return res.status(500).json({ status: "error", message: "Error en la consulta de usuarios" });
+
+        let userIsset = false;
+        users.forEach(user => {
+            if (user && user._id != userIdentity.id) userIsset = true;
+        });
+
+        if (userIsset) {
+            return res.status(200).send({
+                status: "success",
+                message: "El usuario ya existe"
+            });
+        }
+
+        // Cifrar la contraseña
+        if (userToUpdate.password) {
+            let pwd = await bcrypt.hash(userToUpdate.password, 10);
+            userToUpdate.password = pwd;
+
+            //añadido
+        }else{
+            delete userToUpdate.password;
+        }
+
+        // Buscar y actualizar 
+        try {
+            let userUpdated = await User.findByIdAndUpdate({ _id: userIdentity.id }, userToUpdate, { new: true });
+
+            if (!userUpdated) {
+                return res.status(400).json({ status: "error", message: "Error al actualizar" });
+            }
+
+            // Devolver respuesta
+            return res.status(200).send({
+                status: "success",
+                message: "Metodo de actualizar usuario",
+                user: userUpdated
+            });
+
+        } catch (error) {
+            return res.status(500).send({
+                status: "error",
+                message: "Error al actualizar",
+            });
+        }
+
+    });
 }
-// funcion que modifica la foto del usuario
-const uploadImg = ( req, res) => {
-    
+
+const upload = (req, res) => {
+
+    // Recoger el fichero de imagen y comprobar que existe
+    if (!req.file) {
+        return res.status(404).send({
+            status: "error",
+            message: "Petición no incluye la imagen"
+        });
+    }
+
+    // Conseguir el nombre del archivo
+    let image = req.file.originalname;
+
+    // Sacar la extension del archivo
+    const imageSplit = image.split("\.");
+    const extension = imageSplit[1];
+
+    // Comprobar extension
+    if (extension != "png" && extension != "jpg" && extension != "jpeg" && extension != "gif") {
+
+        // Borrar archivo subido
+        const filePath = req.file.path;
+        const fileDeleted = fs.unlinkSync(filePath);
+
+        // Devolver respuesta negativa
+        return res.status(400).send({
+            status: "error",
+            message: "Extensión del fichero invalida"
+        });
+    }
+
+    // Si si es correcta, guardar imagen en bbdd
+    User.findByIdAndUpdate(req.user.id, { image: req.file.filename }, { new: true }).then(userUpdated => {
+        if (!userUpdated) {
+            return res.status(500).send({
+                status: "error",
+                message: "Error en la subida del avatar"
+            })
+        }
+
+        // Devolver respuesta
+        return res.status(200).send({
+            status: "success",
+            user: userUpdated,
+            file: req.file,
+        });
+    });
+
 }
+
+//funcion que devuelve la imágen de un usuario
+const avatar = (req, res) => {
+    // Sacar el parametro de la url
+    const file = req.params.file;
+
+    // Montar el path real de la imagen
+    const filePath = "./uploads/avatars/" + file;
+
+    // Comprobar que existe
+    fs.stat(filePath, (error, exists) => {
+
+        if (!exists) {
+            return res.status(404).send({
+                status: "error",
+                message: "No existe la imagen"
+            });
+        }
+
+        // Devolver un file
+        return res.sendFile(path.resolve(filePath));
+    });
+
+}
+
+// añadido
+const counters = async (req, res) => {
+
+    let userId = req.user.id;
+
+    if (req.params.id) {
+        userId = req.params.id;
+    }
+
+    try {
+
+        let friends = await Friend.count({"user": userId })
+        let friends2 = await Friend.count({"friend": userId})
+        
+
+        const trainings = await Training.count({ "user": userId });
+
+        return res.status(200).send({
+            userId,
+            friends: friends + friends2,
+            trainings
+        });
+    } catch (error) {
+        return res.status(500).send({
+            status: "error",
+            message: "Error en los contadores",
+            error
+        });
+    }
+}
+
+
 
 
 
@@ -211,8 +374,8 @@ module.exports = {
     login,
     profile,
     list,
+    update,
     avatar,
-    deleteUser,
-    modUser,
-    uploadImg
+    upload,
+    counters
 }
--- api/routes/usuario.js
+++ api/routes/usuario.js
@@ -2,8 +2,21 @@
 
 const express = require("express")
 const router = express.Router()
+const multer = require("multer");
 const UserController = require("../controllers/usuario")
 const auth  = require("../middlewares/auth")
+
+// Configuracion de subida
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads/avatars/")
+    },
+    filename: (req, file, cb) => {
+        cb(null, "avatar-"+Date.now()+"-"+file.originalname);
+    }
+});
+
+const uploads = multer({storage});
 
 
 router.get("/prueba", auth.auth, UserController.prueba)
@@ -11,6 +24,11 @@ router.post("/register", UserController.register)
 router.post("/login", UserController.login)
 router.get("/profile/:id", auth.auth, UserController.profile)
 router.get("/list/:page?", auth.auth, UserController.list)
+router.put("/update/:id", auth.auth, UserController.update)
+router.post("/upload", [auth.auth, uploads.single("file")], UserController.upload)
+router.get("/avatar/:file", auth.auth, UserController.avatar)
+router.get("/counters/:id?", auth.auth, UserController.counters)
+
 
 
 module.exports = router
