950200e153bd911804007df7f7bd50fda08c5823
pabloangusto <pabloangusto@gmail.com>
2023-10-19 19:35:46 +0200

    se me olvido

--- api/routes/usuario.js
+++ api/routes/usuario.js
@@ -23,7 +23,6 @@ const uploads = multer({storage});
 router.post("/register", UserController.register)
 router.post("/login", UserController.login)
 router.get("/profile/:id", auth.auth, UserController.profile)
-router.get("/list/:page?", auth.auth, UserController.list)
 router.put("/update/:id", auth.auth, UserController.update)
 router.post("/upload", [auth.auth, uploads.single("file")], UserController.upload)
 router.get("/avatar/:file", auth.auth, UserController.avatar)
