1a77ab5e0a854d4ea5266b6038e4ecffba725d47
Miguel Aréjula Aisa <arejula10@gmail.com>
2023-10-20 21:59:31 +0200

    imagenes vectorizadas

--- web/src/Components/Logo.jsx
+++ web/src/Components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className="logo">
-        <img src="../../public/LETRAS_LOGO_FIT_LIFE.png" className ="img-logo" alt ="FITLIFE"/>
+        <img src="../../public/LogoLetras.png" className ="img-logo" alt ="FITLIFE"/>
     </div>
   )
 }
