7806b2e3e4bcae64ad57c7a5599ae11c18a5f0fb
pabloangusto <pabloangusto@gmail.com>
2023-10-27 13:55:22 +0200

    stats

--- web/src/Components/PageStats.jsx
+++ web/src/Components/PageStats.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import Header from './Header'
+import { useState } from 'react'
+import "../css/PageStats.css"
 
 const PageStats = () => {
+
+    const [sidebar, setSidebar] = useState(false)
+
   return (
     <div className={"page-"+sidebar}>
       <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
       <div className='content'>
         <Header/>
         <div className='principal'>
-          
+            <div className={"stat-"+ sidebar}></div>
+            <div className={"stat-"+ sidebar}></div>
+            <div className={"stat-"+ sidebar}></div>
         </div>
       </div>
     </div>
