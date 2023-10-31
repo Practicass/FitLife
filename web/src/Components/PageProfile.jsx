58e60993b81ee166492651e0e0370a93adcabf21
Miguel Aréjula <arejula10@gmail.com>
2023-10-31 22:36:51 +0100

    hasta amigos

--- web/src/Components/PageProfile.jsx
+++ web/src/Components/PageProfile.jsx
@@ -14,47 +14,38 @@ import Header from "./Header";
  const ProfilePage = () => {
   const [sidebar, setSidebar] = useState(false)
   const {auth} = useAuth()
+ 
   return (
 
-    // <div className={"page-profile-"+sidebar}>
-    //     <Sidebar className="sidebar"sidebar={sidebar} setSidebar={setSidebar}/>
-    //       <div className="content">
-      
-    //         <div className='header-profile'>
-    //           <Logo />
-    //             
-    //         </div>
-    //       <button className=" profile-icon-user">
-    //           {auth.imagen = "default.png" ? 
-    //             <div >
-    //               <FaUserCircle className='default-icon-user' color='#fba92c' size="100px"/> 
-    //             </div>
-    //           : <div>
-    //               <img className='default-img-user' src={auth.imagen}/>
-    //             </div>}
-    //       </button>
-    //     </div>
-    //   </div>
     
 
     <div className={"page-profile-"+sidebar}>
         <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
         <div className='header-profile'>
             <Logo />
-            <div className="settings-logo">
-              <IoMdSettings color='#fba92c' size="50px"/>
+            <div >
+              {/* Add navLink to settings page */}
+              <IoMdSettings className="settings-logo" color='#fba92c' size="50px"/>
             </div> 
-        </div>
-
-        <div className=" profile-icon-user">
+            <div className=" profile-icon-user">
             {auth.imagen = "default.png" ? 
               <div >
                 <FaUserCircle className='default-icon-user' color='#fba92c' size="100px"/> 
               </div>
             : <div>
-                <img className='default-img-user' src={auth.imagen}/>
+                <img className='profile-img-user' src={auth.imagen}/>
               </div>}
+              <h1 className="username">@username{auth.nick}</h1> {/*No funciona lo de auth. nich, revisar como conseguir el nick*/}
+              <p className="friends">AMIGOS</p>
+              <p className="friends-num">80</p>{/*No funciona lo de auth. nich, revisar como conseguir el nick*/}
+              
+            </div>
+            
+           
         </div>
+        
+
+        
     </div>
     
 
