4b911531273e613b655f5590c65661ff8821198f
Miguel Aréjula <arejula10@gmail.com>
2023-11-01 10:01:11 +0100

    login

--- web/src/Components/PageLogin.jsx
+++ web/src/Components/PageLogin.jsx
@@ -50,18 +50,24 @@ const PageLogin = () => {
     }
 
   }
+
+  const style1 = {"borderRadius": "10px", "height":"35px"}
+  const style2 = { "fontSize": "20px", "fontWeight":"bolder"}
+  const style3 = { "fontSize": "20px", "marginTop":"5px", "fontWeight":"bolder"}
+  const style4 = { "fontSize": "15px", "marginTop":"10px"}
+  const style5 = { "fontSize": "20px", "marginTop":"10px", "fontWeight":"bolder"}
   return (
     <div className="pageLogin" >
       <Logo />
       <form className='form-login' onSubmit={loginUser}>
-        <label>Correo Electronico</label>
-        <input type='text' name='email' onChange={changed}></input>
-        <label>Contraseña</label>
-        <input type='password' name='password' onChange={changed}></input>
-        <div>
-          <label>¿No tienes una cuenta?</label><NavLink className="goRegister" to="/register"><label>Registrate</label></NavLink>
+        <label style={style2}>Correo Electronico</label>
+        <input type='text' name='email' onChange={changed} style={style1}></input>
+        <label style={style3}>Contraseña</label>
+        <input type='password' name='password' onChange={changed} style={style1} ></input>
+        <div style={style4}>
+          <label >¿No tienes una cuenta?</label><NavLink className="goRegister" to="/register"><label>Registrate</label></NavLink>
         </div>
-        <MyButton color='orange' type='submit' value="Inicia Sesion">Inicar sesión</MyButton>
+        <MyButton style={style5} color='orange' type='submit' value="Inicia Sesion">Inicar sesión</MyButton>
       </form>
       
     </div>
