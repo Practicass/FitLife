49989277686c86237e6d5d22099bca716808b026
josemifd <josemifd20@gmail.com>
2023-11-03 09:10:15 +0100

    1

---
+++ web/src/Components/PageListFriends.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { useState } from 'react'
+import Sidebar from "./Sidebar"
+import Header from "./Header"
+import { FaUserCircle } from "react-icons/fa"
+import {ImCross} from "react-icons/im"
+import "../css/PageListFriends.css"
+
+const PageListFriends = () => {
+    
+    const[sidebar,setSidebar] = useState(false)
+
+    return (
+        <div className={"page-"+sidebar}>
+            <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
+            <div className='content'>
+                <Header/>
+                <div className="principal">
+                    <form className="busqueda" >
+                        {/* Sustituir esto por una búsqueda */}
+                        <input type="text" name="busqueda" />
+                    </form>
+                    <div className='friend' >
+                        <div className="foto-friend">
+                            <FaUserCircle color='#fba92c' size="50px"/>
+                        </div>
+                        <h2 className="nick-friend"> Pablo </h2>
+                        <div className="cruz">
+                            <ImCross size="25px"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PageListFriends
