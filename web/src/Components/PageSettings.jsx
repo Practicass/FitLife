094497fdf52e73d6e88438dab7d9aacab7acfff5
Miguel Aréjula <arejula10@gmail.com>
2023-11-04 18:31:46 +0000

    avance

--- web/src/Components/PageSettings.jsx
+++ web/src/Components/PageSettings.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/PageSettings.css"
 import Logo from "./Logo";
 import {FaCheck} from "react-icons/fa"
@@ -9,6 +9,22 @@ import {FaUserCircle} from "react-icons/fa"
 
  const PageSettings = () => {
     const {auth} = useAuth()
+
+
+    const email = "EMAIL"
+    const [inputValue, setInputValue] = useState('')    
+    const onInputChange = ({target}) =>{
+        setInputValue(target.value)
+    }
+
+    const onSubmit = (event) => {
+      event.preventDefault();
+      console.log(inputValue)
+      
+      
+    }
+
+
     const styleTitle = {
         "fontSize":"60px",
         "color":"#fba92c", 
@@ -16,7 +32,7 @@ import {FaUserCircle} from "react-icons/fa"
         "marginLeft": "100px"
     }
   return(
-    <div>
+    <div className="settings-page">
 
         <div className="header-settings">
 
@@ -26,15 +42,31 @@ import {FaUserCircle} from "react-icons/fa"
                 </div>
 
             <NavLink to="/profile"><ImCross className="cross-settings"size="35px" color='#fba92c'/></NavLink>
-
-            <FaCheck className="check-settings" color='#fba92c' size="50px"/>
+            {/* Call update to change the user's value */}
+            <NavLink to="/profile"><FaCheck className="check-settings" color='#fba92c' size="50px"/></NavLink>
         </div>
         <div className="principal-settings">
             <h1 style={styleTitle}>MI PERFIL</h1>
+            {/* Call upload to change the user's img */}
             <center><div className="settings-square">
             {auth.imagen = "default.png" ? 
               <FaUserCircle className= "settings-img"color='#fba92c' size="100px" stroke="0"/> 
           : <img className="settings-img"src={auth.imagen}/>}</div></center>
+
+            <form className="settings-form" onSubmit={onSubmit}>
+            <input 
+              type="text"
+              placeholder={email}
+              value={inputValue}
+              onChange={onInputChange}
+
+            
+            
+            />
+
+
+            </form>
+
         </div>
     </div>
   )
