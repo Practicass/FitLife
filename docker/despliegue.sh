b1f137765271afa4117fde8c9f897f61877ea2b6
Miguel Aréjula <arejula10@gmail.com>
2023-12-14 15:52:21 +0100

    docker fin

--- docker/despliegue.sh
+++ docker/despliegue.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 DOCKERFILE_PATH="."  
-IMAGE_NAME1="sisinf/database:latest"
+IMAGE_NAME1="docker-server"
 CONT_NAME1="sisinf-database"
-IMAGE_NAME2="sisinf/web:latest"
+IMAGE_NAME2="docker-client"
 CONT_NAME2="sisinf-web"       
 
-docker stop $CONT_NAME1
-docker rm $CONT_NAME1
+# docker stop $CONT_NAME1
+# docker rm $CONT_NAME1
 
-cd api 
+sudo docker compose down
 
-# Construir la imagen a partir del Dockerfile
-sudo docker build -t $IMAGE_NAME1 $DOCKERFILE_PATH
+# cd api 
 
-#sudo docker run --name $CONT_NAME1 -d $IMAGE_NAME1
+# # Construir la imagen a partir del Dockerfile
+# sudo docker build -t $IMAGE_NAME1 $DOCKERFILE_PATH
 
-cd ..
+# #sudo docker run --name $CONT_NAME1 -d $IMAGE_NAME1
+
+# cd ..
 
 
-docker stop $CONT_NAME2
-docker rm $CONT_NAME2
+# docker stop $CONT_NAME2
+# docker rm $CONT_NAME2
 
-cd web 
+# cd web 
 
-# Construir la imagen a partir del Dockerfile
-sudo docker build -t $IMAGE_NAME2 $DOCKERFILE_PATH
-#sudo docker run --name $CONT_NAME2 --link $CONT_NAME1 -p 3900 -d $IMAGE_NAME2
+# # Construir la imagen a partir del Dockerfile
+# sudo docker build -t $IMAGE_NAME2 $DOCKERFILE_PATH
+# #sudo docker run --name $CONT_NAME2 --link $CONT_NAME1 -p 3900 -d $IMAGE_NAME2
 
 sudo docker compose up -d
 
